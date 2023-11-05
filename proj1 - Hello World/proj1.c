ee0f9c2837d0af0bdba3c4f8e53added1b0d0a32
Rushikesh Malave <149233060+Rushikesh-Malave-183@users.noreply.github.com>
2023-11-05 23:21:13 +0530

    Create proj1.c

---
+++ proj1 - Hello World/proj1.c
@@ -0,0 +1,11 @@
+/*  Date     : 4 July 2023
+    Name     : Rushikesh G. Malave
+    Objective: Print Hello World  */
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
